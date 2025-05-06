07a5b5c1f2f6137dfd965ed45df7d759a0667b6e
Magdi-Khaled <migokhaled88@gmail.com>
2025-05-06 04:16:02 +0300

    feat(new):edit user-item style

--- components/User/Item.vue
+++ components/User/Item.vue
@@ -1,20 +1,20 @@
 <script setup>
 const name = ref('Magdi Khaled');
 const isUserImage = ref(true);
-const show = ref(true);
+const show = ref(false);
 </script>
 
 <template>
-    <div class="p-2 relative border m-1">
+    <div class="p-2 relative border-2 rounded-2xl m-1">
         <UserItemOptions :show="show" />
-        <div class="flex items-center gap-2 cursor-pointer rounded p-2 transition duration-150">
-            <div class="bg-green-300 h-10 w-10 rounded-full flex items-center justify-center font-bold">
+        <div class="w-full h-[60px] flex items-center gap-2 cursor-pointer p-1 transition duration-150">
+            <div class="w-[55px] h-full border rounded-full overflow-hidden flex items-center justify-center font-bold">
                 <span v-if="!isUserImage">{{ name[0] }}</span>
-                <img v-else src="/logo.png" />
+                <img v-else src="@/assets/media/userImage.webp" />
             </div>
-            <div class="flex items-center justify-between w-10/12">
+            <div class="flex items-center justify-between w-11/12">
                 <p class="text-sm font-bold">{{ name }}</p>
-                <Icon name="ic:outline-settings" size="25" class="hover:opacity-[0.85]" @click="show = !show" />
+                <Icon name="mi:options-vertical" size="25" class="hover:opacity-[0.85]" @click="show = !show" />
             </div>
         </div>
     </div>

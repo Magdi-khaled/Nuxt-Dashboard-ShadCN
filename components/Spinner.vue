e9b04337b5dbe952a82e15ace97e3f00f77885e9
Magdi-Khaled <migokhaled88@gmail.com>
2025-05-15 21:36:27 +0300

    feat(update): handle spinner in dark mode

--- components/Spinner.vue
+++ components/Spinner.vue
@@ -4,7 +4,7 @@
 
 <template>
     <div>
-        <p class="animate-spin size-6 relative rounded-full after:absolute after:border-[3px] after:border-neutral-200
+        <p class="animate-spin size-6 relative rounded-full after:absolute after:border-[3px] after:border-neutral-300 dark:after:border-x-neutral-500
         after:border-y-transparent after:rounded-full after:inset-0 mx-4"></p>
     </div>
 </template>

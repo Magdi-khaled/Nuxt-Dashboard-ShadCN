222cf9e3417ff0cf3d4af9d65e530af95d05f555
Magdi-Khaled <migokhaled88@gmail.com>
2025-05-15 21:29:40 +0300

    feat(update): products/id page handle footer dark style

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -10,7 +10,7 @@ const product = computed(() => data.value.find(v => v.productID === route.params
         <h1 class="capitalize font-medium mb-4 border-b border-b-neutral-400 w-fit">product preview</h1>
         <div class="bg-background p-4 border rounded-lg shadow-sm">
             <ProductPreview :product="product" />
-            <p class="text-neutral-600 font-medium w-full text-end sm:pt-0 pt-2">
+            <p class="text-neutral-600 dark:text-neutral-300 font-medium w-full text-end sm:pt-0 pt-2">
                 Product ID: #{{ route.params.id }}</p>
         </div>
     </div>
